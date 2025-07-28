18ec7b82c57dead307944ce294fbae376bf39570
Dimitris <hottorun@pm.me>
2025-07-28 23:21:57 +0300

    Initial Commit

---
+++ MusicNotifier/MusicNotifierApp.swift
@@ -0,0 +1,32 @@
+//
+//  MusicNotifierApp.swift
+//  MusicNotifier
+//
+//  Created by Dimitris Kern on 28.07.25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MusicNotifierApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
